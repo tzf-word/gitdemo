02e0795b713f816593ff38ccb8720af645d32a1d
tzf-word <2625066139@qq.com>
2021-06-28 10:47:28 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * @program: gitdemo
+ * @description:
+ * @author: tzf
+ * @create: 2021-06-28 10:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hellow gitdemo nice you meet you!");
+        System.out.println("hellow gitdemo nice you meet you!");
+        System.out.println("hellow gitdemo nice you meet you!");
+        System.out.println("hellow gitdemo nice you meet you!");
+    }
+}
